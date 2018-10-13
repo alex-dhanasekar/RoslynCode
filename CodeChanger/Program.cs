1af58f10569a1671b42859a53dfd2f24bb41d4b7
DESKTOP-ECVNEF7\Alexander <alexanderimm25@gmail.com>
2018-10-13 16:50:52 +0530

    Code for modifying assignment expressions

--- CodeChanger/Program.cs
+++ CodeChanger/Program.cs
@@ -19,51 +19,86 @@ namespace CodeChanger
             MSBuildLocator.RegisterDefaults();
             var msbws = MSBuildWorkspace.Create();
             var solution = msbws.OpenSolutionAsync(path).Result;
-            var projects = solution.Projects.Where(n=>n.Name.Contains("TestAPP"));
+            var projects = solution.Projects.Where(n => n.Name.Contains("TestAPP"));
             var document = projects.SelectMany(n => n.Documents.Where(x => x.Name == "Form1.Designer.cs")).Single();
             var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
             var tree = document.GetSyntaxTreeAsync().Result;
+
             var compilation = CSharpCompilation.Create("MyCompilation",
-                syntaxTrees: new[] { tree },references: new[] { mscorlib});
+                syntaxTrees: new[] { tree }, references: new[] { mscorlib });
             var root = tree.GetRoot();
-            var initializeComponentMethod = root.DescendantNodes().OfType<MethodDeclarationSyntax>().Where
-                (method=>method.Identifier.ToString().Contains("InitializeComponent")).Single();
-            var memberAccessExpressions = initializeComponentMethod.DescendantNodes()
-                .OfType<MemberAccessExpressionSyntax>().ToList();
             var semanticModel = compilation.GetSemanticModel(tree);
-            string prevType = string.Empty;
-            for (int i = 0; i < memberAccessExpressions.Count; i++) 
-            {
-                //var typeInfo = semanticModel.GetTypeInfo(memberAccessExpressions[i]).Type;
-                //var symbolInfo = semanticModel.GetSymbolInfo(memberAccessExpressions[i]);
-                var lastToken = memberAccessExpressions[i].GetLastToken();
-                if(lastToken.Text == "Text")
-                {
-                    var typeInfo = semanticModel.GetTypeInfo(memberAccessExpressions[i-1]).Type;
-                    if (typeInfo != null && typeInfo.Name.Contains("Label"))
-                    {
-                        Console.WriteLine(memberAccessExpressions[i].GetText());//afafqwfWVWEVWEV
-                        
-                    }
-                }
-            }
+            SyntaxNode newRoot = new MyConsoleWriteRewriter(semanticModel, root).Visit(root);
+            var newDocument = document.WithText(newRoot.GetText());
+            var result = msbws.TryApplyChanges(newDocument.Project.Solution);
+            
             Console.Read();
 
         }
     }
 
-    //class MemberAccessExpressionRewriter : CSharpSyntaxRewriter
-    //{
-    //    public override SyntaxNode VisitMemberAccessExpression(MemberAccessExpressionSyntax node)
-    //    {
-    //        if (!node.IsKind(SyntaxKind.SimpleMemberAccessExpression))
-    //        {
-    //            return base.VisitMemberAccessExpression(node);
-    //        }
+    /// <summary> 
+    /// Replaces Console.Write calls with equivalent 
+    /// Console.WriteLine calls. 
+    /// </summary> 
+    class MyConsoleWriteRewriter : CSharpSyntaxRewriter
+    {
+        private readonly SemanticModel _semanticModel;
+        SyntaxNode _node;
+        public MyConsoleWriteRewriter(SemanticModel model, SyntaxNode node)
+        {
+            _semanticModel = model;
+            _node = node;
+        }
 
-    //        var retVal = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression,
-    //                                                      SyntaxFactory.Literal("NotBaz"));
-    //        return retVal;
-    //    }
-    //}
+        public override SyntaxNode
+            VisitAssignmentExpression(
+                AssignmentExpressionSyntax node)
+        {
+            //    var info = _semanticModel.GetTypeInfo(node.Left).Type;
+            //if (info.Name == "Text" &&
+            //    info.ContainingType.Name == "Label" && 
+            //    info.ContainingNamespace.Name.Contains("Forms")) 
+            if (node.Left.GetLastToken().ToString() == "Text")
+            {
+                var memberAccessExpressions = _node.DescendantNodes().OfType<MemberAccessExpressionSyntax>().ToList();
+                string prevType = string.Empty;
+                for (int i = 0; i < memberAccessExpressions.Count; i++)
+                {
+                    if (memberAccessExpressions[i].GetText().ToString().Trim() == node.Left.GetText().ToString().Trim())
+                    {
+                        var typeInfo = _semanticModel.GetTypeInfo(memberAccessExpressions[i - 1]).Type;
+                        if (typeInfo != null && typeInfo.Name == "Label"
+                            && typeInfo.ContainingType.Name == "Forms")
+                        {
+                            //Console.WriteLine(memberAccessExpressions[i].GetText());//afafqwfWVWEVWEV
+                            var newExpression = SyntaxFactory.ParseExpression(node.GetLeadingTrivia().ToString() + node.Left.ToString() + " = \"New Label 123\"");
+                            return node.ReplaceNode(node, newExpression);
+                        }
+                    }
+                }
+            }
+
+            if (node.Left.GetLastToken().ToString() == "Name")
+            {
+                var memberAccessExpressions = _node.DescendantNodes().OfType<MemberAccessExpressionSyntax>().ToList();
+                string prevType = string.Empty;
+                for (int i = 0; i < memberAccessExpressions.Count; i++)
+                {
+                    if (memberAccessExpressions[i].GetText().ToString().Trim() == node.Left.GetText().ToString().Trim())
+                    {
+                        var typeInfo = _semanticModel.GetTypeInfo(memberAccessExpressions[i - 1]).Type;
+                        if (typeInfo != null && typeInfo.Name == "TextBox"
+                            && typeInfo.ContainingType.Name == "Forms")
+                        {
+                            //Console.WriteLine(memberAccessExpressions[i].GetText());//afafqwfWVWEVWEV
+                            var newExpression = SyntaxFactory.ParseExpression(node.GetLeadingTrivia().ToString() + node.Left.ToString() + " = \"TextBox 567\"");
+                            return node.ReplaceNode(node, newExpression);
+                        }
+                    }
+                }
+            }
+            return node;
+        }
+    }
 }
